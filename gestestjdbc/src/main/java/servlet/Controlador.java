cd08027a103671f6eef6f05c690885b28244348e
FOAP <FOAP@172.28.0.57>
2024-06-28 19:50:44 +0200

    Viernes 28 de junios

--- gestestjdbc/src/main/java/servlet/Controlador.java
+++ gestestjdbc/src/main/java/servlet/Controlador.java
@@ -9,49 +9,79 @@ import jakarta.servlet.http.HttpServletResponse;
 import jdbc.ConecctionJdbc;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Controlador extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	//Declaramos la varaible de la conexión acà!!!!
+
+	// Declaramos la varaible de la conexión acà!!!!
 	private ConecctionJdbc conexionDb;
-       
-   
-    public Controlador() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	
-	public void init(ServletConfig config) throws ServletException {
-		
-		super.init(config);
-		
-		try {
-	        // Intenta cargar el driver JDBC manualmente
-	        Class.forName("com.mysql.cj.jdbc.Driver");
-
-	        // Configuración de la conexión a la base de datos
-	        conexionDb = new ConecctionJdbc("mysql", "localhost", 8889, "dbalumnos", "root", "root", "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
-	        
-	        // Verifica la conexión exitosa imprimiendo un mensaje en la consola
-	        System.out.println("Conexión exitosa!");
-
-	    } catch (ClassNotFoundException e) {
-	        throw new ServletException("MySQL JDBC Driver not found.", e);
-	    } catch (SQLException e) {
-	        throw new ServletException("Error al inicializar la conexión a la base de datos", e);
-	    }
+	public Controlador() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		String parmForm = request.getParameter("id");
-		System.out.println(parmForm);
-		
+	public void init(ServletConfig config) throws ServletException {
+
+		super.init(config);
+
+		try {
+			// Intenta cargar el driver JDBC manualmente
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Configuración de la conexión a la base de datos
+			conexionDb = new ConecctionJdbc("mysql", "localhost", 8889, "dbalumnos", "root", "root",
+					"allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC");
+
+			// Verifica la conexión exitosa imprimiendo un mensaje en la consola
+			System.out.println("Conexión exitosa!");
+
+		} catch (ClassNotFoundException e) {
+			throw new ServletException("MySQL JDBC Driver not found.", e);
+		} catch (SQLException e) {
+			throw new ServletException("Erroooooor al inicializar la conexión a la base de datos", e);
+		}
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Hacer la consulta para recuperar los datos y mostrarlo en el front
+		// Primero hacer para recuperarlos por consola, luego mostrarlos en el navegador
+
+		try {
+			Connection con = conexionDb.getConnection();
+			Statement statement = con.createStatement();
+
+			String query = request.getParameter("sql");
+			ResultSet res = statement.executeQuery(query);
+
+			StringBuilder builder = new StringBuilder();
+
+			while (res.next()) {
+				int id = res.getInt("id");
+				String curso = res.getString("curso");
+				String nombre = res.getString("nombre");
+				builder.append("id: " + id + " curso: " + curso + " nombre: " + nombre + "\n");
+			}
+
+			res.close();
+			statement.close();
+			con.close();
+
+			response.setContentType("text/html");
+			response.getWriter().append(builder.toString());
+
+		} catch (SQLException e) {
+			// Manejar la excepción adecuadamente
+			throw new ServletException("Eeeeeerror al conectar a la base de datos", e);
+		}
+
 	}
 
 }
---
+++ webform/src/main/webapp/js/main.js
@@ -0,0 +1,5 @@
+onload = x;
+
+function x(){
+ document.getElementById("especial").innerHTML ="super proyecto00000";
+ }
